6a4b38c5d20635b84a493ce8c87e287ab1a7b0aa
mishuk09 <mishukinfo09@gmail.com>
2022-11-12 10:20:53 -0500

    function multi

---
+++ Daily_practice/function_multi.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+public class function_multi {
+
+public static int multi(int a, int b){
+
+     int multi=a*b;
+     return multi;
+    
+ 
+}
+
+
+public static void  main(String args[]){
+    Scanner sc= new Scanner(System.in);
+
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+
+    int result=multi(a, b);
+    System.out.println(result);
+}
+
+
+
+
+
+
+
+
+
+    
+}
