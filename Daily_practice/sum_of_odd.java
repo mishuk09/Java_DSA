5b367765a5a097ec34afed73e7bc1afac0b49395
mishuk09 <mishukinfo09@gmail.com>
2022-11-14 11:46:48 -0500

    sum of odd number

---
+++ Daily_practice/sum_of_odd.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class sum_of_odd {
+    public static void main(String args[]){1
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=num;i+=2){
+            sum=sum+i;
+            
+
+        }
+        System.out.println(sum);
+    }
+    
+}
