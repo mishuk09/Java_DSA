25323de7ff39fa4e7a47df034d5b1da7c35a49fc
mishuk09 <mishukinfo09@gmail.com>
2022-11-14 13:05:10 -0500

    circle

--- Daily_practice/circle_func.java
+++ Daily_practice/circle_func.java
@@ -1,3 +1,18 @@
+
+import java.util.*;
+
 public class circle_func {
-    
+    public static int circle(int r) {
+        int circle = (2 * 3 * r);
+        return circle;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+
+        int result = circle(r);
+        System.out.println(result);
+    }
+
 }
