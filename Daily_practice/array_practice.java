2cd748e4201f53a7b06dc435878f992098bec14c
mishuk09 <mishukinfo09@gmail.com>
2022-11-28 13:01:57 -0500

    java daily practice

---
+++ Daily_practice/array_practice.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class array_practice {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        String names[] = new String[size];
+
+        // input
+
+        for (int i = 0; i < size; i++) {
+            names[i] = sc.next();
+        }
+        // output
+        for (int i = 0; i < names.length; i++) {
+
+            System.out.println("Name" + (i) + "is:" + names[i]);
+        }
+
+    }
+
+}
