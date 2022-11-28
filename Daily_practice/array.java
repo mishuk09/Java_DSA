c41d05a66f634bf33089c8a63a68b02a385ca2fc
mishuk09 <mishukinfo09@gmail.com>
2022-11-28 10:03:11 -0500

    java daily practice

---
+++ Daily_practice/array.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class array {
+    public static void main(String args[]) {
+
+        int[] marks = new int[3];
+        marks[0] = 90;
+        marks[1] = 80;
+        marks[2] = 30;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);
+        }
+
+    }
+
+}
