c3499468aa7e0f75c1a7c77e8dd7a0dc4aa397f3
mishuk09 <mishukinfo09@gmail.com>
2022-11-29 07:35:07 -0500

    java

---
+++ Daily_practice/array_2.java
@@ -0,0 +1,10 @@
+public class array_2 {
+    public static void main(String args[]) {
+
+        int marks[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        for (int i = 0; i < 8; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+
+}
