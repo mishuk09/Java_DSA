aca67acabc67ccf52adddcefebe959f1280a9338
mishuk09 <mishukinfo09@gmail.com>
2022-11-30 11:13:24 -0500

    java

---
+++ Daily_practice/substring.java
@@ -0,0 +1,9 @@
+
+public class substring {
+    public static void main(String args[]) {
+        String name = "Mahadi Hasan Mishuk";
+        String name2 = name.substring(4);
+        System.out.println(name2);
+
+    }
+}
