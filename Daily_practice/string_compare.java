fb5de51119656681dbc29922f6e465e5e1357abd
mishuk09 <mishukinfo09@gmail.com>
2022-11-30 10:58:38 -0500

    java

---
+++ Daily_practice/string_compare.java
@@ -0,0 +1,18 @@
+
+
+
+public class string_compare {
+    public static void main(String args[]){
+        String name1="Mahadi";
+        String name2="Hasan";
+
+        if (name1.compareTo(name2)==0){
+            System.out.println("String are equal");
+        }
+        else{
+            System.out.println("String are not equal");
+        }
+
+    }
+    
+}
