5292301b588b3dc39300c0ffa08ec83344ee5ae8
mishuk09 <mishukinfo09@gmail.com>
2022-11-30 10:46:45 -0500

    java

---
+++ Daily_practice/string_input.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class string_input {
+    public static void main(String args[]){
+
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+
+        System.out.println("Your name is=" + name);
+
+
+    }
+    
+    
+}
