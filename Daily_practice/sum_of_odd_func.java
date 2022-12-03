1f7f4964233e335d74b3e2a70da0038e04d16718
mishuk09 <mishukinfo09@gmail.com>
2022-12-03 10:06:30 -0500

    java

--- Daily_practice/sum_of_odd_func.java
+++ Daily_practice/sum_of_odd_func.java
@@ -1,23 +1,21 @@
 import java.util.*;
 
 public class sum_of_odd_func {
-    public static int sum(int num){
-        int sum=0;
-        for(int i=1;i<=num;i+=2){
-            sum=sum+i;
-            
+    public static int sum(int num) {
+        int sum = 0;
+        for (int i = 1; i <= num; i += 2) {
+            sum = sum + i;
 
         }
         return sum;
- 
 
     }
 
-    public static void main (String args[]){
-        Scanner sc=new Scanner(System.in);
-        int num=sc.nextInt();
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
 
-        int result= sum(num);
+        int result = sum(num);
         System.out.println(result);
     }
 }
