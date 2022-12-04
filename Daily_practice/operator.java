507186a9e00c94cff6d1f8d1a1dc8536690c826f
mishuk09 <mishukinfo09@gmail.com>
2022-12-04 08:30:08 -0500

    java

---
+++ Daily_practice/operator.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+public class operator {
+
+    public static void main(String args[]) {
+
+        int num1 = 10;
+        int num2 = 20;
+
+        // +,-,*,/
+
+        int sum = num1 + num2;
+        System.out.println(sum);
+
+        int sub = num2 - num1;
+        System.out.println(sub);
+
+        int mul = num1 * num2;
+
+        System.out.println(mul);
+
+        int div = num2 / num1;
+        System.out.println(div);
+
+        // Boolean
+        
+        if (num1 > num2) {
+            System.out.println(num1);
+
+        }
+
+    }
+
+}
