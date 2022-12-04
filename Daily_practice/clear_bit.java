23719e817fe633b71174b5407ec4b955516b2f9b
mishuk09 <mishukinfo09@gmail.com>
2022-12-04 09:29:03 -0500

    java clear buit

---
+++ Daily_practice/clear_bit.java
@@ -0,0 +1,15 @@
+public class clear_bit {
+
+    public static void main(String args[]) {
+
+        int n = 5;
+        int pos = 2;
+        int bitmask = 1 << pos;
+        int notbitmask = ~(bitmask);
+
+        int newnumber = notbitmask & n;
+
+        System.out.println(newnumber);
+    }
+
+}
