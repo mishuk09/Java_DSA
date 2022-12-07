9bcb003470b8523d114a53cc5a48b6ce248dd1d0
mishuk09 <mishukinfo09@gmail.com>
2022-12-07 07:45:56 -0500

    DSA Bubble sort

--- Daily_practice/bubble_sort.java
+++ Daily_practice/bubble_sort.java
@@ -1,27 +1,29 @@
-package DSA;
 
-public class Bubble_sort {
+public class bubble_sort {
+
     public static void print(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + "");
         }
         System.out.println();
     }
 
-    public static void main(String args[]) {
+    public static void main(string args[]) {
 
-        int arr[] = { 6, 7, 3, 4, 5, 10, 40, 2 };
+        int arr[] = { 7, 2, 9, 5, 2, 1, 4, 8 };
 
-         // bubble sort
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
+
                 if (arr[j] > arr[j + 1]) {
 
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
+
             }
+
         }
 
         print(arr);
