ffe78429fc20b9582dd058511108b9761633256a
mishuk09 <mishukinfo09@gmail.com>
2022-12-07 04:55:20 -0500

    DSA Bubble sort

---
+++ Daily_practice/update_bit.java
@@ -0,0 +1,14 @@
+public class update_bit {
+
+    public static void main(String args[]) {
+
+        int n = 5;
+        int pos = 2;
+        int bitmask = 1 << pos;
+        
+
+        
+    }
+
+    
+}
