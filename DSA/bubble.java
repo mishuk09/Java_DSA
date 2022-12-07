238dd7c52e07fb12573d80070feeafed3bb0bbb4
mishuk09 <mishukinfo09@gmail.com>
2022-12-07 08:36:04 -0500

    DSA Bubble sort practice

--- DSA/bubble.java
+++ DSA/bubble.java
@@ -1,39 +1,33 @@
 
 public class bubble {
-    public static void print(int arr[]){
+    public static void print(int arr[]) {
 
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+ " ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-        //loop for diclare array
+        // loop for diclare array
 
-        int arr[]={1,6,4,9,82,85,35,23};
-        //outer loop
-            for (int i=0;i<arr.length;i++){
-                for(int j=0;j<arr.length-i-1;j++){
+        int arr[] = { 1, 6, 4, 9, 82, 85, 35, 23 };
+        // outer loop
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
 
-                    if (arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-
-
-                    }
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
 
                 }
-            }
-         print(arr);
 
+            }
+        }
+        print(arr);
 
     }
 
-
-
-
-    
 }
