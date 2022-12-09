060acb57cd6f467df9012f15c95d4ac6ea09c325
mishuk09 <mishukinfo09@gmail.com>
2022-12-09 09:23:46 -0500

    DSA slection  sort practice

---
+++ DSA/DSA_Daily_practice/selection_sort.java
@@ -0,0 +1,31 @@
+package DSA.DSA_Daily_practice;
+
+public class selection_sort {
+    public static void fun(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+
+        int arr[] = { 9, 8, 7, 6, 5, 67, 787, 8, 56, 44, 6, 42, 54, 23, 1 };
+
+        for (int i = 0; i < arr.length; i++) {
+            int small = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[small] > arr[j]) {
+                    small = j;
+                }
+            }
+            int temp = arr[small];
+            arr[small] = arr[i];
+            arr[i] = temp;
+
+        }
+        fun(arr);
+    }
+
+}
