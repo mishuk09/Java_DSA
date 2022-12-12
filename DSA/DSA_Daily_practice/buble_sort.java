9d1aad2c7f3f609294d2df1e5ea5f9c3fb3835e6
mishuk09 <mishukinfo09@gmail.com>
2022-12-12 10:57:21 -0500

    DSA bubble  sort practice

---
+++ DSA/DSA_Daily_practice/buble_sort.java
@@ -0,0 +1,28 @@
+package DSA.DSA_Daily_practice;
+
+public class buble_sort {
+    public static void function(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 87, 6, 5, 45645, 634, 32423, 1, 42343, 45346 };
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        function(arr);
+
+    }
+
+}
