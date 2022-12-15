101bfc0aad9770afe2a0609ed6796b6c6fe57784
mishuk09 <mishukinfo09@gmail.com>
2022-12-15 07:46:39 -0500

    java prime number

---
+++ Daily_practice/fact.java
@@ -0,0 +1,24 @@
+public class fact {
+
+    public static void main(String args[]){
+
+        int num=7;
+        int count=0;
+
+        for (int i=2;i<num;i++){
+            if(num%i==0){
+                count++;
+                break;
+            }
+
+        }
+
+        if (count==0){
+            System.out.println("Prime");
+        }
+        else{
+            System.out.println("Not prime");
+        }
+    }
+    
+}
