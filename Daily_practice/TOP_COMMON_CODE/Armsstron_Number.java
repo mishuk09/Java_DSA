2052ad52cd7a1f34eb23c32900f812dfe41b3af9
mishuk09 <mishukinfo09@gmail.com>
2022-12-16 08:58:45 -0500

    java armstrong numvber

---
+++ Daily_practice/TOP_COMMON_CODE/Armsstron_Number.java
@@ -0,0 +1,23 @@
+package TOP_COMMON_CODE;
+
+public class Armsstron_Number {
+    public static void main(String args[]) {
+
+        int num = 153;
+        int arm = 0;
+
+        while (num > 0) {
+            int lastDigit = num % 10;
+            arm = (lastDigit * lastDigit * lastDigit) + arm;
+
+            num = num / 10;
+        }
+
+        if (num == arm) {
+            System.out.println("Armstrong number");
+        } else {
+            System.out.println("Not armstrong  number");
+        }
+    }
+
+}
