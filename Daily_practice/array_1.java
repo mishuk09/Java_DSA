92ef3981cc3f57b620112f3af3c252aeec4e1237
mishuk09 <mishukinfo09@gmail.com>
2022-12-16 07:57:28 -0500

    matrix

--- Daily_practice/array_1.java
+++ Daily_practice/array_1.java
@@ -4,12 +4,12 @@ import java.util.*;
 public class array_1 {
 
     public static void main(String args[]) {
-        Scanner sc=new Scanner(System.in);
-        int size=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
 
         int marks[] = new int[size];
-        for (int i=0;i<size;i++){
-            marks[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            marks[i] = sc.nextInt();
         }
 
         for (int i = 0; i < size; i++) {
