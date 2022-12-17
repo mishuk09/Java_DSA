b7ba9b57b424a95b2ece8dacb8d0b36feb6f1b64
mishuk09 <mishukinfo09@gmail.com>
2022-12-17 07:17:15 -0500

    reverse_number

---
+++ Daily_practice/r.java
@@ -0,0 +1,17 @@
+public class r {
+    public static void main(String args[]) {
+
+        int num = 1234;
+        int rev = 0;
+        while (num > 0) {
+            int lastDigit = num % 10;
+
+            rev = (rev * 10) + lastDigit;
+            num = num / 10;
+
+        }
+        System.out.println(rev);
+
+    }
+
+}
