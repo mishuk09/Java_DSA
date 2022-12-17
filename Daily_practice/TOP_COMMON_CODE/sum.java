39b52d385afc6f43feeb1d63dbc741dbccd4cc75
mishuk09 <mishukinfo09@gmail.com>
2022-12-17 08:14:09 -0500

    Sum of natural number

---
+++ Daily_practice/TOP_COMMON_CODE/sum.java
@@ -0,0 +1,14 @@
+package TOP_COMMON_CODE;
+
+public class sum {
+    public static void main(String args[]) {
+        int num = 5;
+        int sum = 1;
+
+        for (int i = 1; i <= num; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+
+}
