a3a9e62d684e330b51483bf9722af5d333d9a606
mishuk09 <mishukinfo09@gmail.com>
2022-12-17 08:47:45 -0500

    Sum of number by recursion

---
+++ Daily_practice/Recursion/sum.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class sum {
+
+    public static void sum(int a, int b) {
+        int result = a + b;
+        System.out.println(result);
+
+    }
+
+    public static void main(String args[]) {
+
+        sum(10, 20);
+
+    }
+
+}
