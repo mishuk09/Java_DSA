5371ff7a3a8efc542d5be8091f24d045be2bb683
mishuk09 <mishukinfo09@gmail.com>
2022-12-17 07:52:18 -0500

    fibonaccy number

---
+++ Daily_practice/fib.java
@@ -0,0 +1,18 @@
+public class fib {
+
+    public static void main(String args[]) {
+        int num = 10;
+        int a = 0, b = 1, c;
+
+        System.out.print(a + " " + b);
+        for (int i = 1; i <= num; i++) {
+            c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
+
+        }
+
+    }
+
+}
