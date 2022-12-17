698d0ef602a51c2c4557b3c6b9179a1f77dd6380
mishuk09 <mishukinfo09@gmail.com>
2022-12-17 07:12:55 -0500

    factorial number

---
+++ Daily_practice/TOP_COMMON_CODE/f.java
@@ -0,0 +1,18 @@
+package TOP_COMMON_CODE;
+
+public class f {
+    public static void main(String args[]) {
+
+        int num = 5;
+        int fact = 1;
+
+        for (int i = 1; i < num+1; i++) {
+
+            fact = fact * i;
+
+        }
+
+        System.out.print(fact);
+    }
+
+}
