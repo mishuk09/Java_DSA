8032e8648cdfcd65099085d741bce4ff750ab6e2
mishuk09 <mishukinfo09@gmail.com>
2022-12-20 09:09:15 -0500

    Bubble sort

---
+++ Daily_practice/General_code/bubble_sort.java
@@ -0,0 +1,30 @@
+public class bubble_sort {
+
+    public static void function(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 12, 3, 4, 5, 6, 7, 7, 4545, 654, 7565, 8 };
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+
+                }
+
+            }
+        }
+        function(arr);
+
+    }
+
+}
