0d993914e4502af488aec841a973d1074c9438a5
mishuk09 <mishukinfo09@gmail.com>
2022-12-21 11:03:47 -0500

    Fibonacci number

---
+++ Daily_practice/General_code/Fibonacci_num_21_22.java
@@ -0,0 +1,19 @@
+package General_code;
+
+public class Fibonacci_num_21_22 {
+    public static void main(String args[]) {
+
+        int a = 0, b = 1, fibo, num = 10;
+
+        System.out.print(a + " " + b);
+
+        for (int i = 1; i <= num; i++) {
+            fibo = a + b;
+            
+            a = b;
+            b = fibo;
+
+        }
+    }
+
+}
