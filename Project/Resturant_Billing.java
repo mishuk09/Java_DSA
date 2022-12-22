70adaf4c3f46c3a923303b9eead36ec71661fd54
mishuk09 <mishukinfo09@gmail.com>
2022-12-22 12:58:16 -0500

    Project Resruranr Billing system

---
+++ Project/Resturant_Billing.java
@@ -0,0 +1,23 @@
+package Project;
+
+import java.util.*;
+
+public class Resturant_Billing {
+
+    // main p
+    public static void main(String args[]) {
+
+        // print welcome message
+        System.out.println("Welcome to our rasturant");
+        System.out.println();
+        System.out.println("Manue:");
+        System.out.println("1.Tea");
+        System.out.println("2.Cofee");
+        System.out.println("3.Hot cofee");
+
+        Scanner sc = new Scanner(System.in);
+        int ch1 = sc.nextInt();
+
+    }
+
+}
