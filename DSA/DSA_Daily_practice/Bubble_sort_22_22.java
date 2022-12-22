85e49b0d2c89fb89d66305c302c9fa2711eead41
mishuk09 <mishukinfo09@gmail.com>
2022-12-22 12:29:51 -0500

    Bubble sort

--- DSA/DSA_Daily_practice/Bubble_sort_22_22.java
+++ DSA/DSA_Daily_practice/Bubble_sort_22_22.java
@@ -11,9 +11,21 @@ public class Bubble_sort_22_22 {
     }
 
     public static void main(String args[]) {
+
+        // unsorted array
         int arr[] = { 1, 2, 3, 45, 45, 65, 554, 3, 456345, 5324, 24, 564363, 45 };
 
+        System.out.println("Unshorted array");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+        System.out.println("Sorted array");
+
+        // outer loop
         for (int i = 0; i < arr.length - 1; i++) {
+
+            // inner loop
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
 
