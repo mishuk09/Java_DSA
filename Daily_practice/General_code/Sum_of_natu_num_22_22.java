e8e34e9b6e1c48466b6a2f5f942f664aa46ed9d7
mishuk09 <mishukinfo09@gmail.com>
2022-12-22 12:49:32 -0500

    Sum of natural number

---
+++ Daily_practice/General_code/Sum_of_natu_num_22_22.java
@@ -0,0 +1,14 @@
+package General_code;
+
+public class Sum_of_natu_num_22_22 {
+    public static void main(String args[]) {
+        int num = 10;
+        int sum = 0;
+        for (int i = 0; i <= num; i++) {
+            sum = sum + i;
+        }
+
+        System.out.println(sum);
+    }
+
+}
