7080793a8e566d455d180f05c93055547823b941
mishuk09 <mishukinfo09@gmail.com>
2022-12-22 12:44:11 -0500

    GCD number find

---
+++ Daily_practice/General_code/GCD_22_22.java
@@ -0,0 +1,16 @@
+
+package General_code;
+
+public class GCD_22_22 {
+    public static void main(String args[]) {
+        int num1 = 25, num2 = 30, gcd = 0;
+
+        for (int i = 1; i < num1 && i < num2; i++) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                gcd = i;
+            }
+        }
+        System.out.println(gcd);
+    }
+
+}
