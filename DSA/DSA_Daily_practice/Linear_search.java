fa797983e5318d04e4c5f5b72a9980a4d9899583
mishuk09 <mishukinfo09@gmail.com>
2022-12-25 13:43:39 -0500

    Linear search in array

--- DSA/DSA_Daily_practice/Linear_search.java
+++ DSA/DSA_Daily_practice/Linear_search.java
@@ -2,13 +2,21 @@ package DSA.DSA_Daily_practice;
 
 public class Linear_search {
 
+    // Time complexsity of Linear search is O(n)
+
     public static void main(String args[]) {
+
+        // define array
+
         int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 65, 7, 56, 54, 78, 53 };
         int x = 78;
 
+        // loop for read array
         for (int i = 0; i < arr.length; i++) {
+
+            // check x in array or not
             if (x == arr[i]) {
-                System.out.println("X found in(" + i + ")");
+                System.out.println("X found in " + i + " ");
             }
         }
     }
