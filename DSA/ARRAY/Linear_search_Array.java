38ce37badae9db0e086f9e564ed48d89160ba5c1
mishuk09 <mishukinfo09@gmail.com>
2022-12-25 13:25:12 -0500

    array

--- DSA/ARRAY/Linear_search_Array.java
+++ DSA/ARRAY/Linear_search_Array.java
@@ -2,34 +2,40 @@ package DSA.ARRAY;
 
 import java.util.*;
 
-public class TWO_D_Array_25_22 {
+public class Linear_search_Array {
 
     public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
 
-        // Declare row col value
+        // input section
         int row = sc.nextInt();
         int col = sc.nextInt();
+
         // define array
         int arr[][] = new int[row][col];
-        // outer loop for take input
+
+        // taking input
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 arr[i][j] = sc.nextInt();
             }
-
         }
 
-        
+        // define element for search
+
+        int x = sc.nextInt();
+
         // loop for output
-        System.out.println("Output");
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
+                if (x == arr[i][j]) {
+                    System.out.println("X find in(" + i + "," + j + ")");
+                }
 
+            }
+
+        }
     }
+
 }
