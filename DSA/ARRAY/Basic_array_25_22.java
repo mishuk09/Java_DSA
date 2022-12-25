ccff55d7606d663e1cd0d3b9c4d7e24f000fc56b
mishuk09 <mishukinfo09@gmail.com>
2022-12-25 13:55:18 -0500

    Selection sort

--- DSA/ARRAY/Basic_array_25_22.java
+++ DSA/ARRAY/Basic_array_25_22.java
@@ -1,6 +1,6 @@
 package DSA.ARRAY;
 
-public class array_25_22 {
+public class Basic_array_25_22 {
 
     public static void main(String args[]) {
         int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
---
+++ DSA/ARRAY/Selection_sort_array_26_22.java
@@ -0,0 +1,30 @@
+package DSA.ARRAY;
+
+public class Selection_sort_array_26_22 {
+    public static void function(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 76, 77, 858, 78, 77656342, 32, 4412 };
+
+        for (int i = 0; i < arr.length; i++) {
+            int smallEle = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallEle] > arr[j]) {
+                    smallEle = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[smallEle];
+            arr[smallEle] = temp;
+        }
+        function(arr);
+
+    }
+
+}
