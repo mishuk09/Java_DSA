126d6e17aadb9b86f1f2f8096fd7c56e757d00e6
mishuk09 <mishukinfo09@gmail.com>
2022-12-25 10:52:57 -0500

    array

---
+++ DSA/ARRAY/TWO_D_Array_25_22.java
@@ -0,0 +1,29 @@
+package DSA.ARRAY;
+
+import java.util.*;
+
+public class TWO_D_Array_25_22 {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int row = 3;
+        int col = 5;
+
+        int arr[][] = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.println(arr[i][j] + " ");
+            }
+
+        }
+        System.out.println();
+
+    }
+}
