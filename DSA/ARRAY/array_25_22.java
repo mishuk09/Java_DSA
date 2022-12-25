6c2eca3563f169891237c74e2dabe522c6b309df
mishuk09 <mishukinfo09@gmail.com>
2022-12-25 10:28:46 -0500

    array

--- DSA/ARRAY/array_25_22.java
+++ DSA/ARRAY/array_25_22.java
@@ -1,13 +1,12 @@
-public class Array {
+package DSA.ARRAY;
 
-    public static void main(String args) {
+public class array_25_22 {
 
+    public static void main(String args[]) {
         int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
-
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            System.out.print(arr[i]);
         }
-
         System.out.println();
     }
 
