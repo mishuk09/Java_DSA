e40c13a22c3ea121711a09bd9550a78c88916614
mishuk09 <mishukinfo09@gmail.com>
2022-12-25 13:56:38 -0500

    Selection sort

--- DSA/ARRAY/Selection_sort_array_26_22.java
+++ DSA/ARRAY/Selection_sort_array_26_22.java
@@ -1,6 +1,8 @@
 package DSA.ARRAY;
 
 public class Selection_sort_array_26_22 {
+
+    // call function
     public static void function(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -10,8 +12,10 @@ public class Selection_sort_array_26_22 {
     }
 
     public static void main(String args[]) {
+        // define array
         int arr[] = { 1, 2, 3, 4, 5, 6, 76, 77, 858, 78, 77656342, 32, 4412 };
 
+        // outer and inner loop for find element
         for (int i = 0; i < arr.length; i++) {
             int smallEle = i;
             for (int j = i + 1; j < arr.length; j++) {
@@ -19,6 +23,8 @@ public class Selection_sort_array_26_22 {
                     smallEle = j;
                 }
             }
+
+            // swap value
             int temp = arr[i];
             arr[i] = arr[smallEle];
             arr[smallEle] = temp;
