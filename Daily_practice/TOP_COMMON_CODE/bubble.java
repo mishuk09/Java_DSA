7a37d3e55cf8a8741d89ce74a6884d667228a5b7
mishuk09 <mishukinfo09@gmail.com>
2022-12-26 11:45:50 -0500

    Bubble sort practice

---
+++ Daily_practice/TOP_COMMON_CODE/bubble.java
@@ -0,0 +1,29 @@
+package TOP_COMMON_CODE;
+
+public class bubble {
+
+    public static void function(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+
+        int arr[] = { 1, 2, 3, 4, 5, 6, 78, 8 };
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        function(arr);
+    }
+
+}
