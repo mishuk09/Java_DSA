08a4e45e39221387e1eab272d805062f83173271
mishuk09 <mishukinfo09@gmail.com>
2022-12-26 12:13:33 -0500

    Insertion sort

--- Daily_practice/TOP_COMMON_CODE/insertion_sort_26.java
+++ Daily_practice/TOP_COMMON_CODE/insertion_sort_26.java
@@ -4,6 +4,7 @@ import java.util.Currency;
 
 public class insertion_sort_26 {
 
+    // define function
     public static void function(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -13,11 +14,11 @@ public class insertion_sort_26 {
 
     public static void main(String args[]) {
         int arr[] = { 8, 6, 5, 4, 34, 34, 32, 23, 23, 4, 3, 3, 2, 1 };
-
+        // outer lopp for read array
         for (int i = 1; i < arr.length; i++) {
             int smallEle = arr[i];
             int j = i - 1;
-
+            // check condition
             while (j >= 0 && smallEle < arr[j]) {
                 arr[j + 1] = arr[j];
                 j--;
@@ -30,3 +31,5 @@ public class insertion_sort_26 {
     }
 
 }
+
+// Time complexcity of insertion sort O(N^2)
