ca5b95ea4d165847e596fb9682619dd74b975c72
mishuk09 <mishukinfo09@gmail.com>
2022-12-28 14:28:00 -0500

    insert in array

---
+++ DSA/ARRAY/Insert_to_unsorted_array.java
@@ -0,0 +1,17 @@
+package DSA.ARRAY;
+
+public class Insert_to_unsorted_array {
+
+    public static void main(String args[]) {
+
+        int arr[] = { 12, 16, 20, 40, 50, 70 };
+        int x = 26;
+
+        System.out.println("Before insert");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+}
