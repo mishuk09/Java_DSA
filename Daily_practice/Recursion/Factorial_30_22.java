832dc077741a1b0b5d5a1a0e06e68246513e7d04
mishuk09 <mishukinfo09@gmail.com>
2022-12-30 13:32:37 -0500

    recurtion

--- Daily_practice/Recursion/Factorial_30_22.java
+++ Daily_practice/Recursion/Factorial_30_22.java
@@ -1,16 +1,22 @@
 package Recursion;
 
 public class Factorial_30_22 {
-    public static void function(int num){
-if(num==)
-        System.out.println(num);
 
-        function(num);
+    public static int factorialcal(int num) {
+
+        if (num == 1) {
+            return 1;
+        }
+        int fact = factorialcal(num - 1);
+        int fact1 = num * fact;
+        return fact1;
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-        int num=5;
+        int num = 5;
+        int result = factorialcal(num);
+        System.out.println(result);
     }
-    
+
 }
