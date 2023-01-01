474a35a0a47f744033738a377a40506a6040fe0d
mishuk09 <mishukinfo09@gmail.com>
2023-01-01 12:04:14 -0500

    insert element in array

--- DSA/ARRAY/Insert_to_unsorted_array.java
+++ DSA/ARRAY/Insert_to_unsorted_array.java
@@ -1,4 +1,4 @@
-package DSA.ARRAY;
+package ARRAY;
 
 public class Insert_to_unsorted_array {
 
@@ -6,12 +6,25 @@ public class Insert_to_unsorted_array {
 
         int arr[] = { 12, 16, 20, 40, 50, 70 };
         int x = 26;
+        int position = 3;
 
         System.out.println("Before insert");
+
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
 
+        for (int i = arr.length - 1; i > position - 1; i--) {
+
+            arr[i] = arr[i - 1];
+        }
+        arr[position - 1] = x;
+        System.out.println("After insert");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 
 }
