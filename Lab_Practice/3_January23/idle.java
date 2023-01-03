95c249ab42f3c1280ef7eaf8f43ba28f2a6da571
mishuk09 <mishukinfo09@gmail.com>
2023-01-03 12:18:09 -0500

    java

---
+++ Lab_Practice/3_January23/idle.java
@@ -0,0 +1,30 @@
+class index  {
+
+    public static void main(String args[])
+    //static public void main(String[] args)
+    {
+
+        //System.out.println("This is my lab two task");
+
+        //int num=10;
+        //System.out.println("The number is="+ num);
+
+
+int num=101;
+float flo=10.2645363f;
+String name="Mahadi";
+Double dr=476346.486248;
+
+System.out.pritnln((num);
+
+System.out.pritnln(flo);
+
+System.out.pritnln(name);
+
+System.out.pritnln(dr);
+
+
+    }
+
+
+}
