299e07db5eca9bdb9bdb9f8d6a74a27a54223db6
mishuk09 <mishukinfo09@gmail.com>
2023-01-05 13:24:38 -0500

    Binary search

--- DSA/ARRAY/Binary_search.java
+++ DSA/ARRAY/Binary_search.java
@@ -1,14 +1,28 @@
 package ARRAY;
 
+import java.util.*;
+
 public class Binary_search {
 
+
+
+    //Time complexcity if Binary search is O(log n)
+
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // array initial
         int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
-        int x = 12;
+
+        //user input
+        System.out.print("Enter element:");
+        int x = sc.nextInt();
         int smallEle = 0;
         int largeEle = arr.length - 1;
         int midEle = (smallEle + largeEle) / 2;
 
+        // while loop for define limit
         while (smallEle < largeEle) {
             if (arr[midEle] == x) {
                 System.out.println("Element founf in" + " " + midEle + " " + "th Index");
@@ -21,7 +35,7 @@ public class Binary_search {
             midEle = (smallEle + largeEle) / 2;
 
         }
-
+        // if element not found then return message
         if (smallEle > largeEle) {
             System.out.println("Element not found");
         }
