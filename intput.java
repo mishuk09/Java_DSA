c4a1c1704ca9b55ad623740fdcc8b194675f2423
mishuk09 <mishukinfo09@gmail.com>
2023-01-12 09:48:41 -0500

    java

---
+++ intput.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class intput {
+
+    public static void main(String[] args) {
+
+        int firstVariable = 100;
+
+        System.out.println(firstVariable);
+
+    }
+
+}
