bbc08a14dceb7c99979a50f0102e787b38146824
mishuk09 <mishukinfo09@gmail.com>
2023-01-13 08:53:13 -0500

    java

---
+++ arithmetic.java
@@ -0,0 +1,11 @@
+public class arithmetic {
+
+    public static void main(String args[]) {
+        double a = 10;
+        double b = 20;
+
+       double c = a * b;
+        System.out.println(c);
+    }
+
+}
