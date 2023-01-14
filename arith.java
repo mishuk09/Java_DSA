eaed21908921942afe154f0e4cab9ca406f00302
mishuk09 <mishukinfo09@gmail.com>
2023-01-14 12:05:16 -0500

    java

---
+++ arith.java
@@ -0,0 +1,21 @@
+public class arith {
+    public static void main(String args []){
+
+       // int x = 3%2; int y = 4; double z = x/y; System.out.println(z);
+        //double x = 3; double y = 5; int z = x/y; System.out.println(z);
+
+        // int a = 5; int b = 4; int c = 3;                              
+
+        //      int z = a + b - c * a;                                                 
+             
+        //      System.out.println(z);
+
+
+        //int a = 5*6.5;
+        double a = 5*5;
+        System.out.println(a); 
+
+
+    }
+    
+}
---
+++ input.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class input {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int calcius = sc.nextInt();
+        int cal=9*calcius/5+32;
+        System.out.println(cal);
+
+    }
+
+}
