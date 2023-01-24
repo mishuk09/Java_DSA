6d267da43ee46b9f60e5f91686411b38bc118419
mishuk09 <mishukinfo09@gmail.com>
2023-01-24 12:54:47 -0500

    first commit

---
+++ Daily_practice/CLass and object/class.java
@@ -0,0 +1,18 @@
+// package CLass and object;
+
+class temp {
+
+    void display() {
+        System.out.println("Mahadi Hasan Mishuk");
+    }
+
+}
+
+class result {
+    public static void main(String args[]) {
+        temp t = new temp();
+
+        t.display();
+    }
+
+}
---
+++ Daily_practice/General_code/array_marks.java
@@ -0,0 +1,12 @@
+package General_code;
+
+public class array_marks {
+    public static void main(String args[]) {
+
+        int mark[] = { 11, 12, 13, 14, 15, 3 };
+        int roll = mark[mark.length-1];
+        System.out.println(mark[roll]);
+
+    }
+
+}
